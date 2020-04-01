2a061df896924cc9803c767f6aa26eeac3b69cfe
Devforlife07 <Devansh.forlife@gmail.com>
2020-04-01 23:09:07 +0530

    Bugs Fixed

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const route = require('./routes/handlers').route
 
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.engine('hbs', hbs.express4({
     partialsDir: __dirname + '/views/partials'
 }))
 
 app.set('view engine', 'hbs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + "/public"))
 
 
 app.use('/', require('./routes/handlers').route)
